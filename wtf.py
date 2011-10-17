d98ac4bee47ee02ec56d690c4925d1bd839f3331
Alex Clark <aclark@aclark.net>
2011-10-17 18:57:24 -0400

    Show case error

--- wtf.py
+++ wtf.py
@@ -1,6 +1,7 @@
 import sys
+import traceback
 import xmlrpclib
-data = open('screenshot.png').read()
+
 url = 'http://admin:admin@localhost:8080/Plone'
 proxy = xmlrpclib.ServerProxy(url)
 
@@ -15,7 +16,7 @@ proxy = xmlrpclib.ServerProxy(url + '/index.html')
 proxy.setText("""Professionally extend dynamic manufactured products vis-a-vis intermandated niche markets. Proactively leverage existing alternative process improvements and cutting-edge best practices. Efficiently monetize wireless internal or "organic" sources via process-centric potentialities. Quickly administrate strategic functionalities and focused expertise. Dramatically implement fully tested results and distributed human capital. Dynamically pontificate bricks-and-clicks systems with emerging ideas.""")
 
 # create folder
-proxy = xmlrpclib.ServerProxy(url)
+proxy = xmlrpclib.ServerProxy(url)  # reset
 try:
     proxy.invokeFactory('Folder', 'test-folder')
 except xmlrpclib.ProtocolError:
@@ -27,5 +28,17 @@ proxy.setTitle('Test folder')
 proxy.setDescription('This is a test folder')
 
 # create image
-
-
+proxy = xmlrpclib.ServerProxy(url)  # reset
+data = open('screenshot.png').read()
+try:
+    proxy.invokeFactory('Image', 'screenshot.png')
+except xmlrpclib.ProtocolError:
+    print sys.exc_info()[1]
+except xmlrpclib.Fault:
+    print "The id is invalid - it is already in use."  # most likely
+proxy = xmlrpclib.ServerProxy(url + '/screenshot.png')
+proxy.setTitle('This is an image')
+try:
+    proxy.setImage(data)  # XXX this fails
+except:
+    print sys.exc_info()[1]
