e8e0cca81eec0bf7ee73527b8cfdaf23d6b9f5a3
Alex Clark <aclark@aclark.net>
2011-10-17 19:11:24 -0400

    Shorten text

--- wtf.py
+++ wtf.py
@@ -13,7 +13,7 @@ except xmlrpclib.ProtocolError:
 except xmlrpclib.Fault:
     print "The id is invalid - it is already in use."  # most likely
 proxy = xmlrpclib.ServerProxy(url + '/index.html')
-proxy.setText("""Professionally extend dynamic manufactured products vis-a-vis intermandated niche markets. Proactively leverage existing alternative process improvements and cutting-edge best practices. Efficiently monetize wireless internal or "organic" sources via process-centric potentialities. Quickly administrate strategic functionalities and focused expertise. Dramatically implement fully tested results and distributed human capital. Dynamically pontificate bricks-and-clicks systems with emerging ideas.""")
+proxy.setText("""Professionally extend dynamic manufactured products""") 
 
 # create folder
 proxy = xmlrpclib.ServerProxy(url)  # reset
